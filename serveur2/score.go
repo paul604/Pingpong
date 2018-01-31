e61ef664a769c85ac594ffcb793c566231a3aba0
paul <paul604@users.noreply.github.com>
2018-01-31 16:33:21 +0100

    server score OK

--- serveur2/score.go
+++ serveur2/score.go
@@ -1,24 +1,52 @@
 package main
 
 import (
-        "context"
         "github.com/paul604/pingpong/score"
+        "net"
+        "log"
+        "google.golang.org/grpc/reflection"
+        "net/http"
+        "google.golang.org/grpc"
+        "golang.org/x/net/context"
 )
 
 type server struct{}
 
 const (
-        Port = 50051
+        port = "50051"
 )
 
+var scores = map[string]int32{}
+
 //<editor-fold desc="impl SetpongServer">
-func (s *server) Set(context.Context, *score.SetMessage) (*score.SetReply, error) {
-        return nil, nil
+func (s *server) Set(ctx context.Context, msg *score.SetMessage) (*score.SetReply, error) {
+        scores[msg.Nom] = msg.Score
+        return &score.SetReply{}, nil
 }
-func (s *server) Get(context.Context, *score.GetMessage) (*score.GetReply, error) {
-        return nil, nil
+func (s *server) Get(ctx context.Context, msg *score.GetMessage) (*score.GetReply, error) {
+        return &score.GetReply{Score: scores[msg.Nom]}, nil
 }
 func (s *server) Reset(context.Context, *score.ResetMessage) (*score.ResetReply, error) {
-        return nil, nil
+        for key := range scores{
+                scores[key] = 0
+        }
+        return &score.ResetReply{}, nil
 }
 //</editor-fold>
+
+func main() {
+
+        http.ListenAndServe("localhost:"+port, nil)
+
+        lis, err := net.Listen("tcp", port)
+        if err != nil {
+                log.Fatalf("failed to listen: %v", err)
+        }
+        s := grpc.NewServer()
+        score.RegisterScoreServer(s, &server{})
+        // Register reflection service on gRPC server.
+        reflection.Register(s)
+        if err := s.Serve(lis); err != nil {
+                log.Fatalf("failed to serve: %v", err)
+        }
+}
