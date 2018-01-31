ff8500dd2210e34e11bc7e7663cd2f7774951e82
paul <paul604@users.noreply.github.com>
2018-01-31 15:31:41 +0100

    serveur pingpong impl

--- serveur/pingpong.go
+++ serveur/pingpong.go
@@ -1,3 +1,23 @@
 package main
 
+import (
+        "context"
+        "github.com/paul604/pingpong/pingpong"
+)
+
 type server struct{}
+
+//<editor-fold desc="impl PingpongServer">
+func (s *server) Ping(context.Context, *pingpong.PingMessage) (*pingpong.PingReply, error) {
+        return nil, nil
+}
+func (s *server) Pong(context.Context, *pingpong.PongMessage) (*pingpong.PongReply, error) {
+        return nil, nil
+}
+func (s *server) Start(context.Context, *pingpong.StartMessage) (*pingpong.StartReply, error) {
+        return nil, nil
+}
+func (s *server) Stop(context.Context, *pingpong.StopMessage) (*pingpong.StopReply, error) {
+        return nil, nil
+}
+//</editor-fold>
